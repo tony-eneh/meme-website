e6ddf6ce677705c0ba8c498b9e7d6acf566ba468
Tony Eneh <30419946+tony-eneh@users.noreply.github.com>
2023-09-23 10:13:17 +0100

    feat: user can see new meme every 20 secs

--- flasktesto.py
+++ flasktesto.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
+import requests
+import json
+from operator import itemgetter
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Yeah you don land!"
+
+@app.route('/memes')
+def meme():
+    
+    def get_meme():
+        return itemgetter("url", "postLink", "title")(requests.request("GET", "https://meme-api.com/gimme").json())
+
+    url, postLink, title = get_meme()
+    return render_template('index.html', page_title="Ultimate Memes", url = url, postLink = postLink, title = title)
 
 app.run(host = '0.0.0.0', port = 3000)
